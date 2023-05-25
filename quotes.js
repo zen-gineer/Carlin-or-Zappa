9262a1521a38584503b0d02eba0e6380c85526b3
Heather DePesa <heather@depesa.net>
2023-05-25 17:49:51 -0500

    completed

---
+++ quotes.js
@@ -0,0 +1,313 @@
+const localQuotes = [
+  {
+    text: 'Here\'s all you have to know about men and women: women are crazy, men are stupid. And the main reason women are crazy is that men are stupid.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Men are from Earth, women are from Earth. Deal with it.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Fighting for peace is like screwing for virginity.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Some people see things that are and ask, \'Why?\' Some people dream of things that never were and ask, \'Why not?\' Some people have to go to work and don\'t have time for all that.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'The main reason Santa is so jolly is because he knows where all the bad girls live.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Most people work just hard enough not to get fired and get paid just enough money not to quit.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'A house is just a place to keep your stuff while you go out and get more stuff..',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Some people have no idea what they\'re doing, and a lot of them are really good at it.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'The caterpillar does all the work, but the butterfly gets all the publicity.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Never argue with an idiot. They will only bring you down to their level and beat you with experience.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Ever wonder about those people who spend $2 apiece on those little bottles of Evian water? Try spelling Evian backward.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Never underestimate the power of stupid people in large groups.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Careful, if you think too much, they\'ll take you away.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Think off-center.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Think of how stupid the average person is, and realize half of them are stupider than that.',
+    author: '~ George Carlin',
+  },  {
+    text: '"I don\'t have pet peeves. I have major psychotic fucking hatreds.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Life gets really simple once you cut out all the bullshit they teach you in school.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Don\'t just teach your children to read. Teach them to question what they read. Teach them to question everything.',
+    author: '~ George Carlin',
+  },  {
+    text: 'We have multiplied our possessions but reduced our values. We talk too much, love too seldom, and hate too often. We\'ve learned how to make a living but not a life. We\'ve added years to life, not life to years.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Trying to be happy by accumulating possessions is like trying to satisfy hunger by taping sandwiches all over your body.',
+    author: '~ George Carlin',
+  },  {
+    text: 'When fascism comes to America, it will not be in brown and black shirts. It will not be with jack-boots. It will be Nike sneakers and Smiley shirts.',
+    author: '~ George Carlin',
+  },  {
+    text: 'In America, anyone can become president. That\'s the problem.',
+    author: '~ George Carlin',
+  },  {
+    text: 'We are a nation of sheep, and someone else owns the grass.',
+    author: '~ George Carlin',
+  },  {
+    text: 'The IQ and the life expectancy of the average American recently passed each other in opposite directions.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Although I broke a lot of laws as a teenager, I straightened out immediately upon turning eighteen, when I realized the state had a legal right to execute me.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Bullshit is truly the American soundtrack.',
+    author: '~ George Carlin',
+  },  {
+    text: 'When you\'re born into this world, you\'re given a ticket to the freak show. If you\'re born in America you get a front row seat.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Don\'t sweat the petty things and don\'t pet the sweaty things.',
+    author: '~ George Carlin',
+  },  {
+    text: 'I like it when a flower or a little tuft of grass grows through a crack in the concrete. It\'s so fuckin\' heroic.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Electricity is really just organized lightning.',
+    author: '~ George Carlin',
+  },  {
+    text: 'There\'s no present. There\'s only the immediate future and the recent past.',
+    author: '~ George Carlin',
+  },  {
+    text: 'Just cause you got the monkey off your back doesn\'t mean the circus has left town.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'We think in language. The quality of our thoughts and ideas can only be as good as the quality of our language.',
+    author: '~ George Carlin',
+  },
+  {
+    text: '“Inside every cynical person, there is a disappointed idealist.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'When Thomas Edison worked late into the night on the electric light, he had to do it by gas lamp or candle. I\'m sure it made the work seem that much more urgent.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'People who say they don\'t care what people think are usually desperate to have people think they don\'t care what people think.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Most people with low self-esteem have earned it.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'A good motto to live by: \'Always try not to get killed.\'',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'My advice: just keep moving straight ahead. Every now and then you find yourself in a different place.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Everyone smiles in the same language.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Life is a zero sum game.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'If it\'s true that our species is alone in the universe, then I\'d have to say the universe aimed rather low and settled for very little.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'I don\'t have to tell you it goes without saying there are some things better left unsaid. I think that speaks for itself. The less said about it the better.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Have you ever noticed that anybody driving slower than you is an idiot, and anyone going faster than you is a maniac?',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'I went to a bookstore and asked the saleswoman, \'Where\'s the self-help section?\' She said if she told me, it would defeat the purpose.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Some people see the glass half full, others see it half empty. I see a glass that\'s twice as big as it needs to be.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'If you can\'t beat them, arrange to have them beaten.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'One tequila, two tequila, three tequila, floor.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Well, if crime fighters fight crime and firefighters fight fire, what do freedom fighters fight? They never mention that part to us, do they?',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'One good reason to only maintain a small circle of friends is that three out of four murders are committed by people who know the victim.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Instead of warning pregnant women not to drink, I think female alcoholics ought to be told not to fuck.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'What exactly is \'viewer discretion\'? If viewers had discretion, most television shows would not be on the air.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'If black boxes survive air crashes \- why don\'t they make the whole plane out of that stuff?',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'I think the warning labels on alcoholic beverages are too bland. They should be more vivid. Here is one I would suggest: \'Alcohol will turn you into the same asshole your father was.\'',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'I think people should be allowed to do what they want. We haven\'t tried that for a while. Maybe this time it\'ll work.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'To me, authority is something that a freer spirit, a more independent mind, and a person who can handle the world, doesn\'t need guidance frome.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'If you have selfish, ignorant citizens, you\'re going to have selfish, ignorant leaders.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'Conservatives say if you don\'t give the rich more money, they will lose their incentive to invest. As for the poor, they tell us they\'ve lost all incentive because we\'ve given them too much money.',
+    author: '',
+  },
+  {
+    text: 'It\'s called the American dream because you have to be asleep to believe it.',
+    author: '~ George Carlin',
+  },
+  {
+    text: 'It\'s better to have something to remember than anything to regret.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'You can\'t be a real country unless you have a beer and an airline. It helps if you have some kind of a football team, or some nuclear weapons, but at the very least you need a beer.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'A mind is like a parachute. It doesn\.t work if it is not open.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'One size does not fit all.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'Without deviation from the norm, progress is not possible.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'Art is making something out of nothing and selling it.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'There is more stupidity than hydrogen in the universe, and it has a longer shelf life.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'If you wind up with a boring, miserable life because you listened to your mother, your dad, your priest, to some guy on television, to any of the people telling you how to do your shit, then you deserve it.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'Stupidity has a certain charm \- ignorance does not.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'The most important thing to do in your life is to not interfere with somebody else\'s life.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'If you wanna grow up, which most people don\'t, the thing to do is take responsibility for your own reality and deal with it on your own terms.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'I never set out to be weird. It was always other people who called me weird.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'If you\'re going to deal with reality, you\'re going to have to make one big discovery: Reality is something that belongs to you as an individual.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'The more boring a child is, the more the parents, when showing off the child, receive adulation for being good parents — because they have a tame child-creature in their house.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'A true Zen saying: \'Nothing is what I want.\'',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'I like to watch the news, because I don\'t like people very much and when you watch the news… If you ever had an idea that people were really terrible, you could watch the news and know that you\'re right.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'The more you can escape from how horrible things really are, the less it\'s going to bother you… And then, the worse things get.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'A drug is not bad. A drug is a chemical compound. The problem comes in when people who take drugs treat them like a license to behave like an asshole.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'What something is depends more on when it is than anything else.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'There are only two things to remember. Number one… Don\'t Stop, and number two… Keep Going!',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'It gets harder the more you know. Because the more you find out the uglier everything seems.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'The universe consists of 5% protons, 5% neutrons, 5% electrons and 85% morons.',
+    author: '~ Frank Zappa',
+  },
+  {
+    text: 'The United States is a nation of laws: badly written and randomly enforced.',
+    author: '~ Frank Zappa',
+  },
+
+]
--- script.js
+++ script.js
@@ -1,1 +1,72 @@
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const showAuthBtn = document.getElementById('show-auth');
+const loader = document.getElementById('loader');
 
+// Show loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+// Hide loading
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+// Show new quote
+function newQuote() {
+  loading();
+  // Pick a random quote from apiQuotes array
+  const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+  // Hide author initially
+  authorText.classList.add('hide-author');
+  // Check if author is null and replace with 'unknown'
+  if(!quote.author) {
+    authorText.textContent = 'Unknown';
+  } else {
+    authorText.textContent = quote.author;
+  }
+  // Check quote length to determine styling
+  if(quote.text.length > 120) {
+    quoteText.classList.add('long-quote');
+  } else {
+    quoteText.classList.remove('long-quote');
+  }
+// Set quote, hide loader
+  quoteText.textContent = quote.text;
+  complete();
+}
+
+// CODE TO UTILIZE 3RD PARTY API *must change On Load to getQuotes
+// async function getQuotes() {
+//   loading();
+//   const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+//   try {
+//     const response = await fetch(apiUrl);
+//     apiQuotes = await response.json();
+//     newQuote();
+//   } catch (error) {
+//     // Handle error here
+//   }
+// }
+
+// Tweet quote - note the back tics
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+function showAuth() {
+  authorText.classList.remove('hide-author');
+}
+
+// Event listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+showAuthBtn.addEventListener('click', showAuth);
+
+// On load
+newQuote();
